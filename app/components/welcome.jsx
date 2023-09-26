a2d39ff6634e3dc79b9a002d8b88faabf08b01f9
njinsa2 <1t3halilhodzic.sanjin@gmail.com>
2023-09-26 12:48:18 +0200

    Fixed some responsivness

--- app/components/welcome.jsx
+++ app/components/welcome.jsx
@@ -1,11 +1,29 @@
 import Image from 'next/image';
 import React from 'react';
+import { useEffect, useState } from 'react';
 import '../globals.css';
 
 const Welcome = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    setIsMobile(window.innerWidth <= 768);
+    
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+
+  }, []);
+
   return (
-    <div className="background flex flex-col justify-between h-screen">
-      <header className="flex justify-around p-4">
+    <div className="background flex flex-col justify-between h-screen p-4 md:p-0">
+      <header className="flex md:justify-between lg:justify-around p-4">
         <div className="flex items-center">
           <Image src="/assets/relly-black.png" alt="Logo" width={64} height={64} className='hidden lg:block md:block' />
           <p className="text-black  text-2xl  md:mb-2 font-semibold hidden md:block">RELLY</p>
@@ -14,13 +32,13 @@ const Welcome = () => {
       </header>
       <main className="flex-grow flex justify-center">
         <div className="text-center flex flex-col items-center a md:flex-row"> 
-        <p className="text-black  text-2xl md:hidden sm:block font-semibold ">RELLY</p>
-          <Image src="/assets/relly.png" alt="Image" width={350} height={150} className='rellyImg '/>
-          <div className="ml-4"> 
-            <p className="text-black text-2xl tracking-wider font-semibold">A Buddy You Can Rely On. Advice <br></br> You Can Count On.</p>
-            <button className="first-welcome-button ">Try it for free</button>
+        <p className="text-black  text-4xl md:hidden sm:block font-semibold ">RELLY</p>
+          <Image src="/assets/relly.png" alt="Image" width={isMobile ? 275 : 350} height={isMobile ? 275 : 350} className='rellyImg'/>
+          <div className="md:ml-4"> 
+            <p className="text-black text-xl tracking-wider font-semibold mb-3 md:text-2xl md:mr-4 lg:mr-0">A Buddy You Can Rely On. Advice <br></br> You Can Count On.</p>
+            <button className="first-welcome-button">Try it for free</button>
             <br />
-            <button className="second-welcome-button ">Start Experience</button>
+            <button className="second-welcome-button">Start Experience</button>
             <br />
             <button className="text-white md:hidden sm:block mt-2">Already have? <span className='underline'>Sign in!</span></button>
 
