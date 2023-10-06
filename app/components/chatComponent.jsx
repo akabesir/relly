41519762ddd4e6b3a03a709aa2467806b8ad2b59
amar <amarbesirovic@gmail.com>
2023-10-06 11:18:58 +0200

    change urls

--- app/components/chatComponent.jsx
+++ app/components/chatComponent.jsx
@@ -119,7 +119,7 @@ const ChatComponent = () => {
     const fetchChatHistory = async () => {
       try {
         const response = await axios.post(
-          "https://rellyv2.vercel.app/chat/message/get_messages",
+          "https://rellyv2.vercel.app/message/get_messages",
           {
             userId: currentUser.uid,
           }
